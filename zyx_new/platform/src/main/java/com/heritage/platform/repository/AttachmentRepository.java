b6456386472bfefd7502b1d56d65b3fdb385c436
yyxxzz <ivyzxy2005@outlook.com>
2026-04-25 23:43:44 +0800

    chore: 删除多余的 zyx_new 文件夹

--- zyx_new/platform/src/main/java/com/heritage/platform/repository/AttachmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.heritage.platform.repository;
-
-import com.heritage.platform.model.Attachment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AttachmentRepository extends JpaRepository<Attachment, Long> {
-}
